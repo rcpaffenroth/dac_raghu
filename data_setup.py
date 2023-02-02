7d83ea906e420ced6c4fd1e5c015ed11760a4c8f
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-02-02 10:03:47 -0700

    Added better data format.

---
+++ data_setup.py
@@ -0,0 +1,20 @@
+import glob
+import os
+import numpy as np
+
+all_files = glob.glob(os.path.join("data/lti_1d_trajectories", "*.csv"))
+
+data = []
+for file in all_files:
+    data.append(np.loadtxt(file))
+data_1d = np.array(data)
+
+all_files = glob.glob(os.path.join("data/lti_2d_trajectories", "*.csv"))
+
+data = []
+for file in all_files:
+    data.append(np.loadtxt(file))
+data_2d = np.array(data)
+
+np.savetxt('data_1d.txt', data_1d)
+np.savetxt('data_2d.txt', data_2d)
